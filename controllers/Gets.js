c8118185c361e059357b1afc5161a69de648637f
José Donoso <67485758+JoseDonosoValds@users.noreply.github.com>
2024-09-02 15:54:52 -0400

    Monday 15:54
    Se agregaron métodos para tabla "Categorias".
    
    Co-Authored-By: Nicolas Vidal <80706979+nicovidal@users.noreply.github.com>

--- controllers/Gets.js
+++ controllers/Gets.js
@@ -54,6 +54,14 @@ const getPedidos = async (req, res) => {
   }
 };
 
+const getCategoria = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM categoria');
+    res.json(result.rows); 
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
-
-module.exports = {getWaiters,getProduct,getMesa,getAdmin,getPedidos};
+module.exports = {getWaiters,getProduct,getMesa,getAdmin,getPedidos,getCategoria};
--- routes/Restaurant.js
+++ routes/Restaurant.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const {getWaiters, getProduct, getMesa, getAdmin, getPedidos}=require('../controllers/Gets.js')
+const {getWaiters, getProduct, getMesa, getAdmin, getPedidos, getCategoria}=require('../controllers/Gets.js')
 const router = Router();
 
 
@@ -8,5 +8,6 @@ router.get("/product", getProduct);
 router.get("/mesa", getMesa);
 router.get("/admin", getAdmin);
 router.get("/pedidos", getPedidos);
+router.get("/categoria", getCategoria);
 
 module.exports = router;
